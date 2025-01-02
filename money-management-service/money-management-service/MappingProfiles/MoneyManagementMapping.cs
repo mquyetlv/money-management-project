11bff3fac17a15c52a39f7615886f48f1c4e0d67
quyetlv <m.quyetlv@gmail.com>
2025-01-02 23:53:47 +0700

    Mapping Profiles

---
+++ money-management-service/money-management-service/MappingProfiles/MoneyManagementMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using money_management_service.DTOs.ExpenseType;
+using money_management_service.Entities;
+using money_management_service.Extentions;
+
+namespace money_management_service.MappingProfiles
+{
+    public class MoneyManagementMapping : Profile
+    {
+        public MoneyManagementMapping() 
+        {
+            CreateMap<ExpenseType, ExpenseTypeDTO>()
+                .ForMember(dest => dest.StatusName, act => act.MapFrom(src => src.Status.GetDescription()))
+                .ReverseMap();
+        }
+    }
+}
