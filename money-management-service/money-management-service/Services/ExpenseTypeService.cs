83b79d0d51e38c327a8da973f8a01da2545c786b
quyetlv <m.quyetlv@gmail.com>
2025-01-02 23:54:21 +0700

    service

---
+++ money-management-service/money-management-service/Services/ExpenseTypeService.cs
@@ -0,0 +1,44 @@
+﻿using money_management_service.Data;
+using money_management_service.DTOs.ExpenseType;
+using money_management_service.Respository.Interfaces;
+using money_management_service.Services.Interfaces;
+
+namespace money_management_service.Services
+{
+    public class ExpenseTypeService : IExpenseTypeService
+    {
+        private readonly MoneyManagementDBContext _dbContext;
+        private readonly IExpenseTypeRepository _repository;
+
+        public ExpenseTypeService(MoneyManagementDBContext dbContext, IExpenseTypeRepository repository)
+        {
+            _dbContext = dbContext;
+            _repository = repository;
+        }
+
+        public Task<ExpenseTypeDTO> Create(ExpenseTypeDTO expenseTypeDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ExpenseTypeDTO>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ExpenseTypeDTO> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ExpenseTypeDTO> Update(int id, ExpenseTypeDTO entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ money-management-service/money-management-service/Services/Interfaces/IExpenseTypeService.cs
@@ -0,0 +1,13 @@
+﻿using money_management_service.DTOs.ExpenseType;
+
+namespace money_management_service.Services.Interfaces
+{
+    public interface IExpenseTypeService
+    {
+        Task<List<ExpenseTypeDTO>> GetAll();
+        Task<ExpenseTypeDTO> GetById(int id);
+        Task<ExpenseTypeDTO> Create(ExpenseTypeDTO expenseTypeDTO);
+        Task<ExpenseTypeDTO> Update(int id, ExpenseTypeDTO entity);
+        Task Delete(int id);
+    }
+}
