300f034facfc137a7ebaf9e897bf4530273ffb9d
quyetlv <m.quyetlv@gmail.com>
2025-01-20 22:13:38 +0700

    base controller

---
+++ money-management-service/money-management-service/Core/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace money_management_service.Core
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseController : ControllerBase
+    {
+    }
+}
