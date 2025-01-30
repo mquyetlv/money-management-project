a206a36075fadf7a7711ab785501ce77c41753ef
quyetlv <m.quyetlv@gmail.com>
2025-01-30 22:54:27 +0700

    add transaction type repository

---
+++ money-management-service/money-management-service/Respository/TransactionTypeRepository.cs
@@ -0,0 +1,10 @@
+﻿using money_management_service.Data;
+using money_management_service.Entities;
+
+namespace money_management_service.Respository
+{
+    public class TransactionTypeRepository : BaseRepository<TransactionType>
+    {
+        public TransactionTypeRepository(MoneyManagementDBContext dbContext) : base(dbContext) { }
+    }
+}
