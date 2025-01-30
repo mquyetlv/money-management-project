a8771ec446e4788493b196a7fe6de1919c4b239b
quyetlv <m.quyetlv@gmail.com>
2025-01-30 22:54:11 +0700

    add transaction type service

---
+++ money-management-service/money-management-service/Services/Interfaces/ITransactionTypeService.cs
@@ -0,0 +1,15 @@
+﻿using money_management_service.Core;
+using money_management_service.DTOs.TransactionType;
+using money_management_service.Entities;
+
+namespace money_management_service.Services.Interfaces
+{
+    public interface ITransactionTypeService
+    {
+        Task<APIResponse<List<TransactionTypeDTO>>> GetAllAsync(QueryModel<TransactionType> queryModel);
+        Task<APIResponse<TransactionTypeDTO>> GetById(int id);
+        Task<APIResponse<TransactionTypeDTO>> Create(CreateTransactionTypeDTO entity);
+        Task<APIResponse<TransactionTypeDTO>> Update(int id, CreateTransactionTypeDTO entity);
+        Task<bool> Delete(int id);
+    }
+}
---
+++ money-management-service/money-management-service/Services/TransactionTypeService.cs
@@ -0,0 +1,66 @@
+﻿using AutoMapper;
+using money_management_service.Core;
+using money_management_service.DTOs.TransactionType;
+using money_management_service.Entities;
+using money_management_service.Respository.Interfaces;
+using money_management_service.Services.Interfaces;
+
+namespace money_management_service.Services
+{
+    public class TransactionTypeService : ITransactionTypeService
+    {
+        private readonly ITransactionTypeRepository _repository;
+        private readonly IMapper _mapper;
+
+        public TransactionTypeService(ITransactionTypeRepository repository, IMapper mapper) 
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<APIResponse<List<TransactionTypeDTO>>> GetAllAsync(QueryModel<TransactionType> queryModel)
+        {
+            APIResponse<List<TransactionType>> dataResponse = await _repository.GetAllAsync(queryModel);
+            return new APIResponse<List<TransactionTypeDTO>>(_mapper.Map<List<TransactionTypeDTO>>(dataResponse.Data), dataResponse.Pagination);
+        }
+
+        public async Task<APIResponse<TransactionTypeDTO>> GetById(int id)
+        {
+            TransactionType transactionType = await _repository.GetAsync(id);
+            return new APIResponse<TransactionTypeDTO>(_mapper.Map<TransactionTypeDTO>(transactionType));
+        }
+
+        public async Task<APIResponse<TransactionTypeDTO>> Create(CreateTransactionTypeDTO entity)
+        {
+            TransactionType transactionType = new TransactionType()
+            {
+                Name = entity.Name,
+                Description = entity.Description,
+                ExpenseTypeId = entity.ExpenseTypeId,
+            };
+            await _repository.CreateAsync(transactionType);
+            return new APIResponse<TransactionTypeDTO>(_mapper.Map<TransactionTypeDTO>(transactionType));
+        }
+
+        public async Task<APIResponse<TransactionTypeDTO>> Update(int id, CreateTransactionTypeDTO entity)
+        {
+            TransactionType transactionType = await _repository.GetAsync(id);
+            if (transactionType == null)
+            {
+                return null;
+            }
+
+            transactionType.Name = entity.Name;
+            transactionType.Description = entity.Description;
+            transactionType.ExpenseTypeId = entity.ExpenseTypeId;
+            await _repository.UpdateAsync(transactionType);
+            TransactionTypeDTO transactionTypeDTO = _mapper.Map<TransactionTypeDTO>(transactionType);
+            return new APIResponse<TransactionTypeDTO>(transactionTypeDTO);
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            return await _repository.DeleteAsync(id);
+        }
+    }
+}
